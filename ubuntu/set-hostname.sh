26635240d98f5ccafc48af7619312f423005ecd9
yvand <yvan.duron@gmail.com>
2016-11-02 18:19:35 +0100

    Ajout du script set-hostname.sh pour ubuntu

---
+++ ubuntu/set-hostname.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run using sudo, of course
+if [ "$UID" -ne "0" ]; then
+    echo "Il faut être root pour éxecuter ce script. ==> sudo" 1>&2
+    exit 1
+fi
+
+read -p "Nom de la machine : " -e -i "$HOSTNAME" INPUT_HOSTNAME
+hostname $INPUT_HOSTNAME
